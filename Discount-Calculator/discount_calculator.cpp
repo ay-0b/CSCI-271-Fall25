6af357ae794f481ba15c098288585b14b135203b
Ayoub Rannan <ayoubrannan42@gmail.com>
2026-08-20 23:01:58 -0400

    Add files via upload

---
+++ Discount-Calculator/discount_calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int original_price;
+   int discount_percentage;
+
+   discount_percentage = 20;
+   original_price = 100;
+
+   int discount_amount;
+    discount_amount = (original_price * (discount_percentage / 100));
+   int final_price;
+   final_price = original_price - discount_amount;
+   cout<<final_price;
+
+    return 0;
+}
