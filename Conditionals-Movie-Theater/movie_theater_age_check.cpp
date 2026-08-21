52725773bc3707bb658e387c613039368d639496
Ayoub Rannan <ayoubrannan42@gmail.com>
2026-08-20 23:02:25 -0400

    Add files via upload

---
+++ Conditionals-Movie-Theater/movie_theater_age_check.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 0;
+    cout<<"enter your age";
+    cin>>age;
+    if(num>=18){
+        cout<<"You can enter the movie theather";
+    }
+    else{
+        cout<<"You cannot enter the movie theater";
+    }
+    return 0;
+}
