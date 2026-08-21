abe3f80f947587cde841de4e1bb219e9c7b7e2e5
Ayoub Rannan <ayoubrannan42@gmail.com>
2026-08-20 23:02:50 -0400

    Add files via upload

---
+++ Grade-Conditional/grade_conditional.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double score;
+    cout<<"Enter student's score";
+    cin>>score;
+    if (score>=93){
+        cout<<"A";
+
+    }
+
+
+
+    return 0;
+}
