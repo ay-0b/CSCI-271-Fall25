2b4bd50922bf0546868de26b3cd3eca563fa0595
Ayoub Rannan <ayoubrannan42@gmail.com>
2026-08-20 23:01:30 -0400

    Add files via upload

---
+++ Variables/variable_intro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Introduction to Variables
+    //variable declartion: declare the type
+    int score;
+    //variable defition: assign a value
+    cin>>score;
+
+    //use the variable
+    cout<<"My score is "<<score<<endl;
+    return 0;
+}
